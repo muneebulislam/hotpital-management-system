d4a40eb3bbfd9837234b55c47ab592aa2cf6ee71
muneeb <muneebulislam@hotmail.com>
2023-06-03 17:22:16 -0600

    create getter and setters for Person

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,43 @@
+/**
+ * Models a person
+ */
 public class Person {
+    private String name; //Name of the person
+    private int healthNumber; //health number of the person
 
+    /**
+     *
+     * @param name Name of the person
+     * @param healthNumber Health number of the person
+     */
+    public Person(String name, int healthNumber) {
+        this.healthNumber = healthNumber;
+        this.name = name;
+    }
+
+    public int getHealthNumber() {
+        return this.healthNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", healthNumber=" + healthNumber +
+                '}';
+    }
+
+    public static void main(String[] args){
+        Person newPerson = new Person("john", 123);
+        newPerson.setName("kohl");
+                System.out.println(newPerson);
+    }
 }
