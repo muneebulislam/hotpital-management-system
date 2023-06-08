96278214a5549516526f1913a73d9bf9c738c70a
muneebulislam <muneebulislam@hotmail.com>
2023-06-07 23:11:44 -0600

    implement displayEmptyBeds method

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -1,4 +1,5 @@
 import javax.print.Doc;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -12,12 +13,23 @@ public class HospitalSystem {
     private HashMap<String, Doctor> doctors; /** key value map of the doctors */
     private Scanner scanner;
 
+    /**
+     * Constructor for HospitalSystem class
+     * @param wardName String: name of the ward
+     * @param firstBedLabel int: label of the first bed in the ward
+     * @param LastBedLabel int: label of the last bed in the ward
+     */
     public HospitalSystem(String wardName, int firstBedLabel, int LastBedLabel) {
-       ward = new Ward(wardName,firstBedLabel,firstBedLabel);
+       ward = new Ward(wardName,firstBedLabel,LastBedLabel);
        patients = new HashMap<>();
        doctors = new HashMap<>();
        scanner = new Scanner(System.in);
    }
+
+    /**
+     * adds a patient to the hospital system.
+     * @precond user has to enter the name of the patient as well as his health number
+     */
    public void addPatient(){
         int healthNum;
        System.out.println("Enter patient's name: ");
@@ -28,7 +40,7 @@ public class HospitalSystem {
        }
        catch (Exception e){
            System.out.println(e.getMessage());
-           System.out.println("Please enter a number: ");
+           System.out.println("Please enter a valid health number: ");
             healthNum = Integer.parseInt(scanner.nextLine());
        }
 
@@ -36,14 +48,23 @@ public class HospitalSystem {
        patients.put(healthNum,p);
    }
 
+    /**
+     * Adds a doctor to the hospital system.
+     * @precond: User has to enter the
+     */
     public void addDoctor(){
         System.out.println("Enter Doctor's name: ");
         String name = scanner.nextLine();
-        System.out.println("Enter patient's health number: ");
-
         Doctor d = new Doctor(name);
         doctors.put(name,d);
     }
+
+    /**
+     * Assigns a doctor to a patient.
+     * @precond: user has to enter all the information about the doctor as well as the
+     * patient. If the patient and the doctor are already in the hospital system, only
+     * then the doctor can be assigned to the patient.
+     */
     public void assignDoctor(){
         String docName="";
         String patientName="";
@@ -95,8 +116,21 @@ public class HospitalSystem {
         }
 
     }
+
+    /**
+     * Displays empty beds in the ward
+     * @return
+     */
+    public void displayEmptyBeds(){
+        ArrayList<Integer>emptyBeds =  ward.getEmptyBeds();
+        System.out.println("List of Empty beds: "+emptyBeds);
+    }
+
+    /**
+     * A method to run the hospital system.
+     */
    public void run(){
-       String message = "Please Select an option (1-8)" +
+       String message = "Please Select an option (1-8)\n" +
                "1. quit\n" +
                "2. add a new patient to the system\n" +
                "3. add a new doctor to the system\n" +
@@ -106,9 +140,10 @@ public class HospitalSystem {
                "7. release a patient\n" +
                "8. drop doctor-patient association\n";
        int option = -1;
-       while (option!=1){
+       boolean dontEnd = true;
+       while (dontEnd){
            System.out.println(message);
-           System.out.println("\n\nPlease Enter only an integer number (1-8)");
+           System.out.println("\n");
            try{
                option = Integer.parseInt(scanner.nextLine());
            } catch (Exception e) {
@@ -117,12 +152,15 @@ public class HospitalSystem {
            switch (option){
                case 1:
                    scanner.close();
+                   dontEnd = false;
                    break;
                case 2: addPatient();
                         break;
                case 3: addDoctor();
                    break;
                case 4: assignDoctor();
+                   break;
+               case 5: displayEmptyBeds();
                    break;
                default:
                    System.out.println("Please enter a valid number 1-8");
@@ -131,6 +169,10 @@ public class HospitalSystem {
        }
    }
 
+    /**
+     * Main method to test the working of the hospial system.
+     * @param args
+     */
    public static void main(String[] args){
        HospitalSystem hospital = new HospitalSystem("Ward1",10,15);
        hospital.run();
